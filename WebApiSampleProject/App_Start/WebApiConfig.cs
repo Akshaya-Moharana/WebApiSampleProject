f41662b72c64292cc789b44e9e2de51a63f0ba13
Akshaya-Moharana <115500495+Akshaya-Moharana@users.noreply.github.com>
2022-10-12 00:32:43 +0530

    Update WebApiConfig.cs
    Added hardcoded password

--- WebApiSampleProject/App_Start/WebApiConfig.cs
+++ WebApiSampleProject/App_Start/WebApiConfig.cs
@@ -13,6 +13,7 @@ namespace WebApiSampleProject
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+            string passw = "Akshaya@123";
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
